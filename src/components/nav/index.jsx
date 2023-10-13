fa65df4101c1512a15fbc49a5792b0522b419f7b
Reymg22 <117723925+Reymg22@users.noreply.github.com>
2023-10-13 16:06:26 +0200

    initial

---
+++ src/components/nav/index.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Navbar(){
+    return(
+        <nav>
+            <div>
+                <img></img>
+            </div>
+        </nav>
+    )
+}
