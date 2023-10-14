8b0ef35764fefdb84d8c6dc917242f1936b466bc
Reymg22 <117723925+Reymg22@users.noreply.github.com>
2023-10-14 12:54:11 +0200

    Navbar Added

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,12 +1,36 @@
-import './index.css'
+import "./index.css";
 import React from "react";
-import { Link } from "react-router-dom";
-export default function Navbar(){
-    return(
-        <nav>
+import Logo from '../../assets/images/logo_img.png'
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="container-nav">
+        <div className="imageContainer">
+          <img src={Logo} alt="Logo" />
+        </div>
+        <ul>
+          <li>
             <div>
-                <img></img>
+              <a href="#accueil">Accueil</a>
             </div>
-        </nav>
-    )
+          </li>
+          <li>
+            <div>
+              <a href="#a-propos">A propos</a>
+            </div>
+          </li>
+          <li>
+            <div>
+              <a href="#portfolio">Portfolio</a>
+            </div>
+          </li>
+          <li>
+            <div>
+              <a href="#contact">Contact</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../../containers/Header';
+import './index.css'
+export default function Home() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
