49db0f6cbe2dc809e007945111a05e734272c8b3
Reymg22 <117723925+Reymg22@users.noreply.github.com>
2023-10-15 14:17:26 +0200

    Header Updated & AboutMe Added

---
+++ src/components/about/index.jsx
@@ -0,0 +1,23 @@
+import './index.css';
+import Coding from '../../assets/images/web-developer-coding-img.jpg'
+export default function AboutMe(){
+    return(
+        <section className='aboutContainer'>
+        <div className='containAll'>
+        <article className='aboutContent'>
+        <div className='about-title' id='a-propos'>
+        <h2>A propos de moi</h2>
+        </div>
+        <div className='about-text'>
+        <p>En tant que développeur web spécialisé dans le développement frontend, je possède une expertise solide dans un éventail de langages et technologies, notamment HTML, CSS, JavaScript, SCSS, Node.js et React.js. Mon expérience me permet de concevoir des designs web attrayants tout en optimisant les sites pour une excellente expérience utilisateur sur tous les appareils. Mon objectif est de créer des interfaces réactives qui garantissent une navigation fluide. Je suis prêt à mettre en œuvre mes compétences pour contribuer de manière significative à votre équipe de développement frontend.</p>
+        </div>
+        </article>
+        <div className='aboutPhotos'>
+        <div className='imgContainer'>
+        <img src={Coding} alt="web-developer-coding"/>
+        </div>
+        </div>
+        </div>
+        </section>
+    )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../../containers/Header';
+import AboutMe from '../../components/about'
 import './index.css'
 export default function Home() {
   return (
     <div>
       <Header />
+      <main>
+      <AboutMe />
+      </main>
     </div>
   );
 }
