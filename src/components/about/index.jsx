3e396d9b02271a3843b2763833f22119f83a7bd7
Reymg22 <117723925+Reymg22@users.noreply.github.com>
2023-11-11 16:52:25 +0100

    Aboutme Page English & French translation Added

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,23 +1,29 @@
+// AboutMe.js
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Coding from '../../assets/images/web-developer-coding-img.jpg';
 import './index.css';
-import Coding from '../../assets/images/web-developer-coding-img.jpg'
-export default function AboutMe(){
-    return(
-        <section className='aboutContainer'  id='a-propos'>
-        <div className='containAll'>
+
+export default function AboutMe() {
+  const { t } = useTranslation();
+
+  return (
+    <section className='aboutContainer' id='a-propos'>
+      <div className='containAll'>
         <article className='aboutContent'>
-        <div className='about-title'>
-        <h2>A propos de moi</h2>
-        </div>
-        <div className='about-text'>
-        <p>En tant que développeur web spécialisé dans le développement frontend, je possède une expertise solide dans un éventail de langages et technologies, notamment HTML, CSS, JavaScript, SCSS, Node.js et React.js. Mon expérience me permet de concevoir des designs web attrayants tout en optimisant les sites pour une excellente expérience utilisateur sur tous les appareils. Mon objectif est de créer des interfaces réactives qui garantissent une navigation fluide. Je suis prêt à mettre en œuvre mes compétences pour contribuer de manière significative à votre équipe de développement frontend.</p>
-        </div>
+          <div className='about-title'>
+            <h2>{t('AboutTitle')}</h2>
+          </div>
+          <div className='about-text'>
+            <p>{t('AboutText')}</p>
+          </div>
         </article>
         <div className='aboutPhotos'>
-        <div className='imgContainer'>
-        <img src={Coding} alt="web-developer-coding"/>
+          <div className='imgContainer'>
+            <img src={Coding} alt="web-developer-coding"/>
+          </div>
         </div>
-        </div>
-        </div>
-        </section>
-    )
+      </div>
+    </section>
+  );
 }
