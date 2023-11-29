22d51af748d6124d1b354c3995e814c9ae81bc1d
Reymg22 <117723925+Reymg22@users.noreply.github.com>
2023-11-29 12:57:03 +0100

    Update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../../containers/Header/homepage/index";
+import Header from "../../containers/Header/HomePage/index";
 import AboutMe from "../../components/about";
 import Projects from "../../components/projects/index";
 import HomeContact from "../../components/contact/index";
